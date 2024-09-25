2b106040caed69f8d65360b08e9b3106455ad3ca
Niki5225 <122698681+Niki5225@users.noreply.github.com>
2024-09-25 15:55:03 +0300

    making the code cleaner and adding lose functionality

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 'use strict';
 const submitBtn = document.querySelector('.check');
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
 const againBtn = document.querySelector('.again');
 
 let message = document.querySelector('.message');
@@ -9,6 +8,11 @@ let score = document.querySelector('.score');
 let currentScore = 20;
 let highScore = document.querySelector('.highscore');
 let secretNumberElement = document.querySelector('.number');
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+const displayMessage = function (messageSent) {
+  message.textContent = messageSent;
+};
 
 // Submit button event handler
 submitBtn.addEventListener('click', function () {
@@ -16,23 +20,23 @@ submitBtn.addEventListener('click', function () {
 
   // Handle invalid input
   if (guess <= 0 || guess > 20) {
-    message.textContent = 'Not a correct input ⛔';
-    // Handle higher number case
-  } else if (guess > secretNumber) {
-    message.textContent = 'Too high 📈';
-    currentScore--;
-    score.textContent = currentScore;
-
-    // Handle higher number case
-  } else if (guess < secretNumber) {
-    message.textContent = 'Too low 📉';
-    currentScore--;
-    score.textContent = currentScore;
+    displayMessage('Not a correct input ⛔');
+    // Handle number guess incorrect case
+  } else if (guess !== secretNumber) {
+    if (currentScore > 1) {
+      displayMessage(guess > secretNumber ? 'Too high 📈' : 'Too low 📉');
+      currentScore--;
+      score.textContent = currentScore;
+      // Check if player lost
+    } else {
+      displayMessage('You lost the game 💥💥💥');
+      score.textContent = 0;
+    }
 
     // Handle coincidence case
   } else if (guess === secretNumber) {
     body.style.backgroundColor = 'green';
-    message.textContent = 'Congrats! You found the secret number ! 🎉🎉🎉';
+    displayMessage('Congrats! You found the secret number ! 🎉🎉🎉');
     let currentHighScore = Number(highScore.textContent);
     secretNumberElement.textContent = secretNumber;
     secretNumberElement.style.width = '30rem';
@@ -48,11 +52,12 @@ submitBtn.addEventListener('click', function () {
 // Again button handler
 againBtn.addEventListener('click', function () {
   // Resets everything so that the user can start the game again and aim for better score :)
-  message.textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   const guess = document.querySelector('.guess');
   guess.value = '';
   score.textContent = 20;
   body.style.backgroundColor = '#222';
   secretNumberElement.style.width = '15rem';
   secretNumberElement.textContent = '?';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
 });
